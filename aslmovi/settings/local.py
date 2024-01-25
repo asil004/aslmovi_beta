40ed9c7499d7af24c9fbc585830f771cabc1645b
asil004 <72306088+asil004@users.noreply.github.com>
2024-01-25 19:15:30 +0500

    beta 4

--- aslmovi/settings/local.py
+++ aslmovi/settings/local.py
@@ -5,13 +5,13 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
-    'default': {
-        "SQL_ENGINE": "django.db.backends.postgresql_psycopg2",
-        "SQL_DATABASE": "aslmovi",
-        "SQL_USER": "postgres",
-        "SQL_PASSWORD": 12345,
-        "SQL_HOST": "db",
-        "SQL_PORT": 5432,
+    "default": {
+        "ENGINE": config("SQL_ENGINE"),
+        "NAME": config("SQL_DATABASE"),
+        "USER": config("SQL_USER"),
+        "PASSWORD": config("SQL_PASSWORD"),
+        "HOST": config("SQL_HOST"),
+        "PORT": config("SQL_PORT"),
     }
 }
 
